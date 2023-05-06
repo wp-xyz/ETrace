53f5eca96af5d7b53c29e4b36bc53dadc98393a4
wp_xyz <wp_xxyyzzz@gmx.net>
2023-05-06 14:42:15 +0200

    New unit et_Sim

---
+++ src/ed_sim.pas
@@ -0,0 +1,20 @@
+unit ed_sim;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils,
+  et_Global, et_Math, et_Objects;
+
+type
+  TSimulation = class
+  end;
+
+implementation
+
+
+
+end.
+
